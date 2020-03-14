759b2794cb8202598113e3582250f0f4834a6a9c
Prog-ZeinabAli <for.programers@gmail.com>
2020-03-14 20:01:27 +0300

    Initial Commit

---
+++ jsonPost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jsonPost
+//
+//  Created by test1 on 3/14/20.
+//  Copyright © 2020 test1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
